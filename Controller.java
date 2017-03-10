c31d813e1b1c0ad803d83c43a6a4dccae6f40e96
Viktor Ekström <mongomoves@gmail.com>
2017-03-10 14:43:44 +0100

    Lägger till en tidig controller

---
+++ Controller.java
@@ -0,0 +1,36 @@
+package guMeddelandesystem;
+
+import javax.swing.*;
+
+public class Controller {
+	private String ip, name;
+	private int port;
+//	private Client client;
+	private UI ui;
+	
+	public Controller() {
+		ui = new UI(this);
+	}
+	
+	public void readIp(String ip) {
+		this.ip = ip;
+	}
+	
+	public void readPort(int port) {
+		this.port = port;
+	}
+	
+	public void readUsername(String name) {
+		this.name = name;
+	}
+	
+	public void sendImage(String filepath) {
+		ImageIcon image = new ImageIcon(filepath);
+		ui.displayImage(image);
+	}
+	
+	public void connectToServer() {
+		//client.connect(this.ip, this.port, this.name);
+	}
+
+}
