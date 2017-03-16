228013c90f701cfe0c04515aa0d554fba8748af1
Ejiik <Erik.johansson_93@hotmail.com>
2017-03-16 17:41:41 +0100

    Update MsgServer

--- MsgServer.java
+++ MsgServer.java
@@ -10,7 +10,8 @@ import java.util.LinkedList;
 
 public class MsgServer {
 	private int port;
-	private LinkedList<Object> msgBuffer = new LinkedList<Object>();
+	private LinkedList<String> userReg = new LinkedList<String>();
+	private LinkedList<Message> msgBuffer = new LinkedList<Message>();
 
 	public MsgServer(int port) {
 		this.port = port;
@@ -79,26 +80,47 @@ public class MsgServer {
 		}
 
 		public void run() {
-			Object[] msg = null;
+			Message msg = null;
+			String username = null;
+			String[] tempUserReg = (String[])userReg.toArray();
+			
+			try{
+				for(int i=0; i<tempUserReg.length; i++){
+					if(ois.readObject().equals(tempUserReg[i])){
+						username = (String)ois.readObject();
+						break;
+					}else{
+						if(i == tempUserReg.length-1){
+							username = (String)ois.readObject();
+							userReg.add(username);
+							break;
+						}
+					}
+						
+				}
+			} catch(Exception e){
+				e.printStackTrace();
+			}
+			
 			while (true) {
 				try {
-					if (ois.readObject() instanceof Object[]) {
-						msg = (Object[]) ois.readObject();
-						if (msg[0] instanceof InetAddress) {
-							msgBuffer.add(msg);
-						} else {
-							oos.writeUTF("Invalid InetAddress: " + msg[0]);
-						}
+					
+					
+					if (ois.readObject() instanceof Message) {
+						msg = (Message)ois.readObject();
+					
+						msgBuffer.add(msg);
+						
 					}
 
 					if (ois.readObject() instanceof String) {
 						if (ois.readObject().equals("getMsgBuffer")) {
-							Object[] messagesTemp = new Object[msgBuffer.size()];
+							Message[] messagesTemp = new Message[msgBuffer.size()];
 		
-							messagesTemp = msgBuffer.toArray();
+							messagesTemp = (Message[]) msgBuffer.toArray();
 
 							for (int i = 0; i < messagesTemp.length; i++) {
-								if (!messagesTemp[i].equals(this.clientAddress)) {
+								if (!messagesTemp[i].getReceivers().contains(username)) {
 									messagesTemp[i] = null;
 
 								}
@@ -117,13 +139,14 @@ public class MsgServer {
 									nbrOfMessages++;
 								}
 							}
-							Object[] messages = new Object[nbrOfMessages];
+							Message[] messages = new Message[nbrOfMessages];
 							
 							for(int i = 0; i<nbrOfMessages; i++){
 								messages[i] = messagesTemp[i];
 							}
 							oos.writeObject(messages);
-						}
+							
+						} 
 					}
 				} catch (ClassNotFoundException e) {
 					e.printStackTrace();
